a1792a26066df01b835bff2c6b317e7c3471ec25
rickytsangdev <rickytsang.dev@gmail.com>
2025-03-21 19:53:00 +0100

    add router outlet for handle routing in app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LibraryComponent } from './pages/library/library.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-export const routes: Routes = [{}, {}, {}];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'library', component: LibraryComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
