f83d2145898af5040a91d62294f90ed67d97d59e
rickytsangdev <rickytsang.dev@gmail.com>
2025-03-21 19:36:52 +0100

    create pages structures
    start with login,signup, notfound, library, product

--- src/app/shared/atoms/button/button.component.ts
+++ src/app/shared/atoms/button/button.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './button.component.html',
-  styleUrl: './button.component.scss'
+  styleUrl: './button.component.scss',
 })
 export class ButtonComponent {
-
+  @Input() type: string = 'default';
 }
