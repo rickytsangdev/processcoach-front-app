9f51bf1f7522b659d0b1f5c2516ea11586d5fd3d
rickytsangdev <rickytsang.dev@gmail.com>
2025-04-05 22:55:53 +0200

    badgeChecklist added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
-import { NavbarComponent } from "../../shared/organisms /navbar/navbar.component";
+import { NavbarComponent } from '../../shared/organisms /navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [NavbarComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   welcomePage: string = './assets/welcomePage.png';
-  badgeCertification:string = './assets/badgeCertifcation.png'
-
+  badgeCertification: string = './assets/badgeCertifcation.png';
+  badgeChecklist: string = './assets/badgeChecklist.png';
 }
