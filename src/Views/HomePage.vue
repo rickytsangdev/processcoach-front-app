8731aae88a990268d486bc7f629219a1252e6f9d
rickytsangdev <rickytsang.dev@gmail.com>
2025-04-11 17:18:10 +0200

    create  routing in our app

---
+++ src/Views/HomePage.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+  data() {
+    return {}
+  },
+}</script>
+
+<template>
+  <h1>Je suis la Home Page</h1>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+
+// import page here
+import HomePage from '@/Views/HomePage.vue'
+import CataloguePage from '@/Views/CataloguePage.vue'
+import LoginPage from '@/Views/auth/LoginPage.vue'
+import RegisterPage from '@/Views/auth/RegisterPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'Home',
+      component: HomePage,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/catalogue',
+      name: 'Catalogue',
+      component: CataloguePage,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: LoginPage,
+    },
+    {
+      path: '/signup',
+      name: 'Singup',
+      component: RegisterPage,
     },
   ],
 })
 
 export default router
+
+// {
+//   path: '/about',
+//   name: 'about',
+//   // route level code-splitting
+//   // this generates a separate chunk (About.[hash].js) for this route
+//   // which is lazy-loaded when the route is visited.
+//   component: () => import('../views/AboutView.vue'),
+// },
