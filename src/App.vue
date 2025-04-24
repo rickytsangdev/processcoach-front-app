124a2cd0ae1a3ab224bcc525156bf6ea0b5032f2
rickytsangdev <rickytsang.dev@gmail.com>
2025-04-24 15:57:55 +0200

    install daisyUI for design components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <script setup>
-  import { RouterLink, RouterView } from 'vue-router'
-  import NavBar from './components/shared/NavBar.vue';
-
+import { RouterLink, RouterView } from 'vue-router'
+import NavBar from './components/shared/NavBar.vue'
 </script>
 
 <template>
-  <NavBar/>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <NavBar />
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <button class="btn">Hello daisyui</button>
 
-<div class="container">
-    <Router-view/>
+  <div class="container">
+    <Router-view />
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
