f86e34c969c7c0f79fa7674c54ddcf4aa7c7ac05
rickytsangdev <rickytsang.dev@gmail.com>
2025-04-25 17:31:55 +0200

    add dashboard user and dashboard profile page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,8 @@ import { LibraryComponent } from './pages/library/library.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './pages/user/dashboard/dashboard.component';
+import { ProfileComponent } from './pages/user/profile/profile.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -12,6 +14,8 @@ export const routes: Routes = [
   { path: 'library', component: LibraryComponent },
   { path: 'products', component: ProductsComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'user/dashboard', component: DashboardComponent },
+  { path: 'user/profile', component: ProfileComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ];
