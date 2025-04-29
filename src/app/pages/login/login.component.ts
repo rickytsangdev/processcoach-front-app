c78eeb1219002457e341fd01ad157e9f9afd355c
rickytsangdev <rickytsang.dev@gmail.com>
2025-04-29 18:48:15 +0200

    fix : add link to signup in login component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormComponent } from '../../shared/molecules/form/form.component';
 import { UpperCasePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormComponent, UpperCasePipe],
+  imports: [FormComponent, UpperCasePipe, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
