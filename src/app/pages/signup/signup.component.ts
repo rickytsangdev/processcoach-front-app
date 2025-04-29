0838c76588028f18b815a5fcf4bc2d4227e1baf9
rickytsangdev <rickytsang.dev@gmail.com>
2025-04-29 18:17:17 +0200

    create first validators to handle form

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,29 +1,31 @@
 import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { RouterLink, Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { FormComponent } from '../../shared/molecules/form/form.component';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [RouterLink, FormComponent, ReactiveFormsModule],
+  imports: [RouterLink, FormComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './signup.component.html',
 })
 export class SignupComponent {
   formType: 'login' | 'signup' = 'signup';
   action: string = "S'inscrire";
   fb = inject(FormBuilder)
+  json : any
 
   form = this.fb.nonNullable.group({
-    email: [''], 
-    password :[''],
-    lastName : [''], 
-    firstName : [''],
+    email: ['', [Validators.required, Validators.minLength(3)]], 
+    password :['', Validators.required],
+    lastName : ['', Validators.required], 
+    firstName : ['', Validators.required],
   })
 
   onSubmit() {
-    console.log(this.form.value); // Récupère les valeurs du formulaire
+    console.log(this.form.value);
+    console.log(this.form.controls.email.errors); // Récupère les valeurs du formulaire
   }
 
   constructor(private router: Router) {}
