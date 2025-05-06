048dc042a87e2c1da8d21d22fde6dcc85129ca96
rickytsangdev <rickytsang.dev@gmail.com>
2025-05-06 16:12:23 +0200

    add header condition :  hide header in login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet, ActivatedRoute } from '@angular/router';
+import { Router, RouterOutlet, ActivatedRoute } from '@angular/router';
 import { HeaderComponent } from './shared/organisms /header/header.component';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [ RouterOutlet, HeaderComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
+  constructor(private route: ActivatedRoute, public router: Router) {}
 
-  constructor(private route: ActivatedRoute) {
+  ngOnInit(): void {
+    console.log(this.route.snapshot);
+  }
+  title = 'training.processcoach-front';
 
-
-}
-
-ngOnInit(): void {
-  console.log(this.route.snapshot);
-  
-}
-title = 'training.processcoach-front';
+  shouldShowHeader(): boolean {
+    return this.router.url !== '/login';
+  }
 }
--- src/app/shared/organisms /header/header.component.ts
+++ src/app/shared/organisms /header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -9,6 +8,7 @@ import { RouterLink } from '@angular/router';
 })
 export class HeaderComponent {
   document!: any;
+  
 
   goToUrl(): void {
     this.document.location.href = 'https://processcoach.fr';
