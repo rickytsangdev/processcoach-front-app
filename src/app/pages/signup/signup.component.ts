f6c807e37f850066a48b390d508f0c190c8025ce
rickytsangdev <rickytsang.dev@gmail.com>
2025-05-08 18:05:16 +0200

    firebase auth : storing user data in firestore

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -12,6 +12,7 @@ import { FormComponent } from '../../shared/molecules/form/form.component';
 import { CommonModule } from '@angular/common';
 import { Auth } from '@angular/fire/auth';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-signup',
@@ -27,6 +28,7 @@ export class SignupComponent {
   #auth = inject(Auth);
   alertMsg: any;
   alertColor: any;
+  #firestore= inject(Firestore)
 
   form = this.fb.nonNullable.group({
     email: [
@@ -52,15 +54,21 @@ export class SignupComponent {
     console.log(this.form.controls.email.errors);
     console.log(this.form.controls['password'].errors);
 
-    const { email, password, lastName, firstName } = this.form.getRawValue();
+    const { email, password } = this.form.getRawValue();
 
     try {
-      // Récupère les valeurs du formulaire
+      // Récupère les valeurs du formulaire la stock dans une variable auth qui l'injecte via le cdk firebase directement
       const userCred = await createUserWithEmailAndPassword(
         this.#auth,
         email,
         password
       );
+
+      // on s'occupe ici de sauvegarder les autres éléments du formulaire
+      await addDoc(collection(this.#firestore, 'users'), {
+        firstName : this.form.getRawValue().lastName, 
+        lastName : this.form.getRawValue().firstName
+      })
 
       console.log(userCred);
     } catch (error) {
