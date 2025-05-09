c04ce56704119f631f6d84060dce3c1ee2c7cd3a
rickytsangdev <rickytsang.dev@gmail.com>
2025-05-09 17:03:57 +0200

    Plateform Navigation : add more pages and management routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,8 +9,14 @@ import { DashboardComponent } from './pages/user/dashboard/dashboard.component';
 import { ProfileComponent } from './pages/user/profile/profile.component';
 import { AboutComponent } from './pages/about/about.component';
 import { CourseComponent } from './pages/course/course.component';
+import { CertificateComponent } from './pages/user/certificate/certificate.component';
+import { AdminMenuComponent } from './pages/admin/admin-menu/admin-menu.component';
+import { FaqComponent } from './pages/faq/faq.component';
+import { AnalysisComponent } from './pages/admin/analysis/analysis.component';
+import { UserListComponent } from './pages/admin/user-list/user-list.component';
 
 export const routes: Routes = [
+  // open routes
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'library', component: LibraryComponent },
@@ -18,8 +24,16 @@ export const routes: Routes = [
   { path: 'products/:id', component: CourseComponent },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'faq', component: FaqComponent },
+  // user signin routes
   { path: 'dashboard', component: DashboardComponent },
   { path: 'user/profile', component: ProfileComponent },
+  { path: 'user/certificate', component: CertificateComponent },
+  // admin panel routes
+  { path: 'admin/menu', component: AdminMenuComponent },
+  { path: 'admin/analysis', component: AnalysisComponent },
+  { path: 'admin/user-list', component: UserListComponent },
+  // rediection routes
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: '**', component: NotFoundComponent },
 ];
