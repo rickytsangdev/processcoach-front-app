b5c4d7316242b911bc2ef470b95c98fd71c6bb6f
rickytsangdev <rickytsang.dev@gmail.com>
2025-05-14 10:25:11 +0200

    fix span : class visibility

--- src/app/pages/user/profile/profile.component.ts
+++ src/app/pages/user/profile/profile.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [RouterLink, RouterOutlet],
+  imports: [RouterLink, RouterOutlet, CommonModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
 })
 export class ProfileComponent {
+  currentUrl: string = '';
 
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.currentUrl = this.router.url;
+    });
+    this.currentUrl = this.router.url;
+  }
 }
