b1068ed18b957eb3ea8f9d73754dd6472bc811e4
rickytsangdev <rickytsang.dev@gmail.com>
2025-05-14 15:52:08 +0200

    page profile : stats => create ui with basics  KPI for users

--- src/app/pages/user/profile/stats/stats.component.ts
+++ src/app/pages/user/profile/stats/stats.component.ts
@@ -5,8 +5,17 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './stats.component.html',
-  styleUrl: './stats.component.scss'
+  styleUrl: './stats.component.scss',
 })
 export class StatsComponent {
-
+  stats = {
+    coursesStarted: 18,
+    coursesCompleted: 9,
+    totalTime: '13h',
+    certificates: 3,
+    lastCourse: {
+      title: 'Développement Web 3',
+      lastAccess: 'il y a 4 jours',
+    },
+  };
 }
