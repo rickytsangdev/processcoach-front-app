6aa1993fb01d56402fdd828509a29b4820b511ef
rickytsangdev <rickytsang.dev@gmail.com>
2025-05-14 15:35:44 +0200

    add template for certificate UI

--- src/app/pages/user/profile/certificate/certificate.component.ts
+++ src/app/pages/user/profile/certificate/certificate.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-certificate',
   standalone: true,
   imports: [],
   templateUrl: './certificate.component.html',
-  styleUrl: './certificate.component.scss'
+  styleUrl: './certificate.component.scss',
 })
 export class CertificateComponent {
-
+  @Input() fileName_1 = 'attestation-entree-de-formation.pdf';
+  @Input() fileName_2 = 'attestation-fin-de-formation.pdf';
+  @Input() downloadUrl = '';
 }
